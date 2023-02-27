9452618dee2d0deb99618e942ca5c361b6a53308
Eilis9 <G00006088@atu.ie>
2023-02-27 14:04:45 +0000

    week5 assignment rev1

---
+++ weekday.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Week 5 assignment
+Program which prints if the current day is a weekday or weekend
+Author: Eilis Donohue
+Ref: PYnative.com/python-get-the-day-of=week
+"""
+
+from datetime import datetime
+# assign the current date and time to dt
+dt = datetime.now()
+
+# weekday method to find day of week - returns integer
+x = dt.weekday()
+
+# checks value of x - prints accordingly
+if x == 6 or x == 7:
+    print("It is the weekend, yay!")
+else:
+    print("Yes, unfortunately today is a weekday.")
