81df05b995c3360f2830f30a12d5276c5cb52ada
Eilis9 <G00006088@atu.ie>
2023-03-08 18:56:00 +0000

    accounts.py tidy

--- accounts.py
+++ accounts.py
@@ -1,13 +1,13 @@
-# accounts.py
-# Author: Eilis Donohue
-# Week 3 task - asks for a 10 char input and outputs only the last 4 digits
-
+'''accounts.py
+Week 3 PANDS task assignment 
+Author: Eilis Donohue
+Asks for a 10 char input and outputs only the last 4 digits
+'''
 
 # Ask the user for 10 digit number
 user_input = input("Please enter a 10 digit account number: ")
 
-# user_input = " 0001234567890"
-# strip any whitespace from start and finish - maybe remove later (perhaps add an error check instead).
+# strip any whitespace from start and finish - maybe remove later 
 input_stripped = user_input.strip()
 
 # find the length of the input string (no spaces)
@@ -16,16 +16,12 @@ input_string_length = len(input_stripped)
 # check if all the characters are digits  - not used right now
 all_digits_chk = input_stripped.isdigit()
 
-#print("digit check is", all_digits_chk)
-
 # Using a slice for now to get the last 4 chars of the input string
 output_string = input_stripped[-4:]
 
-# pad the output with Xs depending on the length of the user input  - inputs less than 4 digits will output just that input
+# pad the output with Xs depending on the length of the user input  
+# - inputs less than 4 digits will output just that input
 output_string_any = input_stripped[-4:].rjust(input_string_length, 'X')
 
-# print the account number with 6 Xs and just the last 4 digits inputted. This assumes a 10 digit input (ignoring trailing whitespaces)
-print("XXXXXX" + output_string)
-
-# print statement for account numbers of any length (inputs more than 4 digits will have leading digits padded with Xs)
+# Print statement for account numbers of any length 
 print(output_string_any)
